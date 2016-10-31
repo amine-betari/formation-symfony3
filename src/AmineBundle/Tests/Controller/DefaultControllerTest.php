ad19f31ab98f2e2f5ad51d7c56f749955b81ebeb
amine.betari <amine.betari@gmail.com>
2016-10-31 15:31:40 +0100

    Test Functionnel & Unit

--- src/AmineBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AmineBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,7 +8,30 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testContact()
     {
+		// Cette méthode retourne un client, qui est comme un navigateur
+		/*
+		$client = static::createClient(array(
+			'environment' = > 'my_test_env',
+			'debug'       = > false,
+		));
+		*/
         $client = static::createClient();
+		// La méthode request retourne un objet Crawler qui peut être utilisé pour sélectionner des éléments dans la réponse
+		// Le crawler ne fonctionne que lorsque la réponse est un fichier XML ou un document HTML
+		// Pour obtenir la réponse du contenu brut, appelez $client->getResponse()->getContent()
+		// La signature de la méthode request est :
+		/*
+		request(
+			$method,
+			$uri,
+			array $parameters = array(),
+			array $files = array(),
+			array $server = array(),
+			$content = null,
+			$changeHistory = true
+		)
+		*/
+		// les méthodes 
         $crawler = $client->request('GET', '/nous-contacter');
 		$this->assertEquals(1, $crawler->filter('h1:contains("Contact Amine")')->count());
 
